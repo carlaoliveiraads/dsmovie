adae67140fbc9ebd121bff5f3442c2e14db8e726
Carla Oliveira <carlaoliveira.ads@gmail.com>
2022-03-12 22:16:21 -0300

    ajuste no MovieCard

--- frontend/src/components/Navbar/MovieScore/index.tsx
+++ frontend/src/components/Navbar/MovieScore/index.tsx
@@ -1,4 +1,4 @@
-import MovieStars from "../MovieStars";
+import MovieStars from "components/Navbar/MovieStars";
 import './styles.css';
 
 function MovieScore() {
@@ -6,16 +6,13 @@ function MovieScore() {
     const score = 3.5;
     const count = 13;
 
-
-  return (
-    <div className="dsmovie-score-container">
-        <p className="dsmovie-score-value">
-            {score > 0 ? score.toFixed(1) : "-"}
-        </p>
-        <MovieStars/>
-        <p className="dsmovie-score-count">{count} avaliações</p>
-    </div>
-  );
+    return (
+        <div className="dsmovie-score-container">
+            <p className="dsmovie-score-value">{score > 0 ? score.toFixed(1) : '-'}</p>
+            <MovieStars />
+            <p className="dsmovie-score-count">{count} avaliações</p>
+        </div>
+    );
 }
 
 export default MovieScore;
